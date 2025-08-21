98eb542b3d0d07851ebdbad124ad9c4794a35cd8
Renan Ricoldi <renan.ricoldi.frois@gmail.com>
2025-08-20 22:49:29 -0300

    feat: enable cors from any origin

--- src/main.ts
+++ src/main.ts
@@ -32,10 +32,10 @@ async function bootstrap() {
     jsonDocumentUrl: '/api/docs/json',
   });
 
-  // app.enableCors({
-  //   origin: '*',
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  // });
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
   app.use(cookieParser());
   app.use(
     helmet({
